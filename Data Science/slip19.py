af7824cde25c4f4aa1265c7dfca5103a9dd6ea3e
sajid-shaikh-77 <sajid.shaikh@fladdra.com>
2022-03-14 13:00:22 +0530

    added remaining slips

--- Data Science/slip19.py
+++ Data Science/slip19.py
@@ -1,18 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.array([1, 2, 3, 4])
-y = x*x
-
-# first plot with X and Y data
-plt.plot(x, y)
-
-x1 = ["Sajid", "Yasin", "Sahil", "Sameer"]
-y1 = [83, 75, 67, 79]
-
-# second plot with x1 and y1 data
-plt.plot(x1, y1, '-.')
-
-plt.xlabel("Name")
-plt.ylabel("Percentage")
+x= ["Sajid", "Yasin", "Sahil", "Sameer"]
+y= [83, 85, 82, 84]
+plt.plot(x, y, color="red")  
+plt.xlabel("Name")  # add X-axis label
+plt.ylabel("percentage")  # add Y-axis label
+plt.title("Line Chart")  # add title
 plt.show()
--- Web & Php/slip19-20/test.php
+++ Web & Php/slip19-20/test.php
@@ -17,23 +17,23 @@ foreach($array3 as $y=>$y_value)
 echo "Age of ".$y." is : ".$y_value."
 ";
 }
-echo "
-Associative array : Descending order sorting by Value
-";
-$age=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
-arsort($age);
-foreach($age as $y=>$y_value)
-{
-echo "Age of ".$y." is : ".$y_value."
-";
-}
-echo "
-Associative array : Descending order sorting by Key
-";
-$array4=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); krsort($array4);
-foreach($array4 as $y=>$y_value)
-{
-echo "Age of ".$y." is : ".$y_value."
-";
-} 
 ?>
+<!-- // echo "
+// Associative array : Descending order sorting by Value
+// ";
+// $age=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40");
+// arsort($age);
+// foreach($age as $y=>$y_value)
+// {
+// echo "Age of ".$y." is : ".$y_value."
+// ";
+// }
+// echo "
+// Associative array : Descending order sorting by Key
+// ";
+// $array4=array("Sophia"=>"31","Jacob"=>"41","William"=>"39","Ramesh"=>"40"); krsort($array4);
+// foreach($array4 as $y=>$y_value)
+// {
+// echo "Age of ".$y." is : ".$y_value."
+// ";
+// }  -->
